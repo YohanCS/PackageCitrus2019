86446d57cb5995e5063d07fa9f7a03f065b6c943
Yohan <yhezkiya@ucsd.edu>
2019-04-27 03:31:32 -0700

    take picture takes one picture now

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ function cameraStart() {
         console.error("Oops. Something is broken.", error);
     });
 }
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+    //cameraOutput.src = cameraSensor.toDataURL("image/webp");
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
