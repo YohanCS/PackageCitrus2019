4ae83b6c3358546b4fdfc0940025333242000a5d
Yohan <yhezkiya@ucsd.edu>
2019-04-27 11:59:07 -0700

    got button click to register on server

--- app.js
+++ app.js
@@ -3,14 +3,60 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const nodeWebCam = require('node-webcam');
+const fs = require('fs');
 
+// specifying parameters for the pictures to be taken
+var options = {
+    width: 1280,
+    height: 720, 
+    quality: 100,
+    delay: 1,
+    saveShots: true,
+    output: "jpeg",
+    device: false,
+    callbackReturn: "location"
+};
 
+// create instance using the above options
+var webcam = nodeWebCam.create(options);
 
-router.get('/',function(req,res){
+// capture function that snaps <amount> images and saves them with the given name in a folder of the same name
+var captureShot = (amount, i, name) => {
+    var path = `./images/${name}`;
+
+    // create folder if and only if it does not exist
+    if(!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    } 
+
+    // capture the image
+    webcam.capture(`./images/${name}/${name}${i}.${options.output}`, (err, data) => {
+        if(!err) {
+            console.log('Image created')
+        }
+        console.log(err);
+        i++;
+        if(i <= amount) {
+            captureShot(amount, i, name);
+        }
+    });  
+};
+
+app.use(bodyParser.json());
+captureShot(1, 1, 'ga2ry');
+
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
+app.post('/', function (req, res) {
+    console.log('button clicked');
+  });
+
+app.use(express.static('./assets/css/images/'));
 app.use(express.static('./'));
 
 //add the router
--- camera.js
+++ camera.js
@@ -19,11 +19,13 @@ function cameraStart() {
 }
 
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+/*cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     //cameraOutput.src = cameraSensor.toDataURL("image/webp");
-};
+
+ 
+}; */ 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
