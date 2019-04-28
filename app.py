42a9d011eaf341057e6c63224671c61a58748f22
Yohan <yhezkiya@ucsd.edu>
2019-04-27 23:52:51 -0700

    reverted changes for cam

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import cv2
 from google.cloud import vision
 import io
+import re # for regular expressions in getting label 
+import time # for sleep
 
 app = Flask(__name__)
 
@@ -20,38 +22,46 @@ def detect_labels(path):
 
     response = client.label_detection(image=image)
     labels = response.label_annotations
-    print('Labels:')
+    #print('Labels:')
 
+    regex = "red|luggage|bag"
     for label in labels:
-        print(label)
+        desc = label.description
+        if(desc == 'Bag' or desc == 'Red' or desc == 'Luggage'
+            or desc == 'Backpack'):
+            print("YAy a match!")
+        #else:
+            #print("aww not a match")
+        #print(label.description)
 
 def loggedIn():
     cam = cv2.VideoCapture(0)
 
-    cv2.namedWindow("test")
+    cv2.namedWindow("security")
 
     img_counter = 0
 
     while True:
-        ret, frame = cam.read()
-        cv2.imshow("test", frame)
+        (ret, frame) = cam.read()
+        cv2.imshow("security", frame)
+        
+        # if the frame was not grabbed, then we have reached the end
+        # of the stream 
         if not ret:
             break
-        k = cv2.waitKey(1)
+        k = cv2.waitKey(5000)
 
-        if k%256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-        elif k%256 == 32:
+        #if k%256 == 27:
+         #   # ESC pressed
+         #   print("Escape hit, closing...")
+         #   break
+        #elif k%256 == 32:
             # SPACE pressed
-            img_name = "luggage{}.png".format(img_counter)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            detect_labels('luggage'+str(img_counter) + '.png')
-            print()
-            print()
-            img_counter += 1
+        img_name = "luggage{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        detect_labels('luggage'+str(img_counter) + '.png')
+        img_counter += 1
 
     cam.release()
 
